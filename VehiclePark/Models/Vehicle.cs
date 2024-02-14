db1325a4fe085e1fd7e3877eb43938de887f54ed
robinruis <robin.ruis@gmail.com>
2024-02-14 10:17:36 +0100

    Added the model and dc

---
+++ VehiclePark/Models/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VehiclePark.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public required string LicensePlate { get; set; }
+        public required string Color { get; set; }
+        public int BuildYear { get; set; }
+        public string? LoanedTo { get; set; }
+        public StatusOptions Status { get; set; }
+        public string? Comments { get; set; }
+    }
+
+    public enum StatusOptions
+    {
+        [Display(Name = "Lent out")]
+        Lentout,
+
+        [Display(Name = "Available")]
+        Available,
+
+        [Display(Name = "In repair")]
+        InRepair,
+
+        [Display(Name = "Sold")]
+        Sold,
+
+        [Display(Name = "In order")]
+        InOrder
+    }
+}
